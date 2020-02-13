6fd0eeb9c2e60ba46b54873213074eae971825f1
Naomi <naomipriest@rocketmail.com>
2020-02-13 09:09:34 -0500

    Finished

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -31,24 +31,9 @@ let menuItems = [
   function createMenu(menuItems){
     const menu = document.createElement('div');
     const listTitle = document.createElement('ul');
-    const listText = document.createElement('li');
-    const listText2 = document.createElement('li');
-    const listText3 = document.createElement('li');
-    const listText4 = document.createElement('li');
-    const listText5 = document.createElement('li');
-    const listText6 = document.createElement('li');
-
-
-    //end created elements
+   
+    
     menu.appendChild(listTitle);
-    listTitle.appendChild(listText);
-    listTitle.appendChild(listText2);
-    listTitle.appendChild(listText3);
-    listTitle.appendChild(listText4);
-    listTitle.appendChild(listText5);
-    listTitle.appendChild(listText6);
-
-
     const menuButton = document.querySelector('.menu-button');
 
     menu.classList.add('menu');
@@ -57,15 +42,12 @@ let menuItems = [
       menu.classList.toggle('menu--open');
     })
 
-    //console.log(menuItems);
+    
     menuItems.map(data =>{
+    const listText = document.createElement('li');
     listText.textContent = data;
-    listText2.textContent = data;
-    listText3.textContent = data;
-    listText4.textContent = data;
-    listText5.textContent = data;
-    listText6.textContent = data;
-
+    listTitle.appendChild(listText);
+    
   });
 
  
